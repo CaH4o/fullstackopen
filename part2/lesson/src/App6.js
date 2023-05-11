660494017c8f1ef7b8a70aab51631c6301d50870
Oleksandr Tertyshnyk <aleksandr.tertyshnyk@gmail.com>
2023-05-11 21:31:28 +0300

    Part2: updeted README and lessins project with the 'd' subpart

---
+++ part2/lesson/src/App6.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import noteService from "./services/notes";
+import Note from "./components/Note";
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    // noteService.getAll().then((response) => setNotes(response.data));
+    noteService.getAll().then((initialNotes) => setNotes(initialNotes));
+  }, []);
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    };
+
+    noteService
+      .create(noteObject)
+      //.then((response) => {
+      //  setNotes(notes.concat(response.data));
+      .then((returnedNote) => {
+        setNotes(notes.concat(returnedNote));
+        setNewNote("");
+      });
+  };
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) =>
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)))
+      )
+      .catch((error) => {
+        alert(`the note '${note.content}' was already deleted from server`);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
+    //.then((response) =>
+    //  setNotes(notes.map((note) => (note.id !== id ? note : response.data)))
+    //);
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={(event) => setNewNote(event.target.value)}
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
--- part2/lesson/src/index.js
+++ part2/lesson/src/index.js
@@ -5,7 +5,9 @@ import ReactDOM from "react-dom/client";
 //import App from "./App1";
 //import App from "./App2";
 //import App from "./App3";
-import App from "./App4";
+//import App from "./App4";
+//import App from "./App5";
+import App from "./App6";
 
 // --1--
 /* const notes = [
