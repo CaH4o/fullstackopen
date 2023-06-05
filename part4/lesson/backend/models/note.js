b10df68c5bea49412e539c2f2cecbae070eaf454
Oleksandr Tertyshnyk <aleksandr.tertyshnyk@gmail.com>
2023-06-05 11:31:06 +0300

    Part4:a: Updated README and lessons project

--- part4/lesson/backend/models/note.js
+++ part4/lesson/backend/models/note.js
@@ -1,36 +1,20 @@
-const mongoose = require("mongoose");
-
-mongoose.set("strictQuery", false);
-
-const url = process.env.MONGODB_URI;
-
-//console.log("connecting to", url);
-
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
-  //content: String,
   content: {
     type: String,
     minLength: 5,
-    required: true
+    required: true,
   },
   important: Boolean,
-});
+})
 
-noteSchema.set("toJSON", {
+noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Note", noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
