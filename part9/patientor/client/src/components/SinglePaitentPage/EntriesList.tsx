34ee8a3ebfb54a81373e779613ae99366ac811ce
Oleksandr Tertyshnyk <aleksandr.tertyshnyk@gmail.com>
2023-11-16 00:43:55 +0200

    Part9:e - Completed exercise 9.24

--- part9/patientor/client/src/components/SinglePaitentPage/EntriesList.tsx
+++ part9/patientor/client/src/components/SinglePaitentPage/EntriesList.tsx
@@ -1,44 +1,59 @@
+import { useState, useEffect } from 'react';
 import { Stack, Typography, Box } from '@mui/material';
 import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material/';
+
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 
-import { Entry } from '../../types';
+import { Diagnosis, Entry } from '../../types';
+import diagnosService from '../../services/diagnos';
 
 interface Props {
   entries: Entry[];
 }
 
 const EntriesList = ({ entries }: Props) => {
+  const [diagnos, setDiagnos] = useState<Diagnosis[]>([]);
+
+  useEffect(() => {
+    const fetchDiagnosisList = async () => {
+      const diagnos = await diagnosService.getAll();
+      setDiagnos(diagnos);
+    };
+    void fetchDiagnosisList();
+  }, [entries]);
+
   return (
     <Stack>
       <Typography variant='h6' style={{ margin: '1em 0', fontWeight: 'bold' }}>
-        {entries.length ? 'entries' : null}
+        entries
       </Typography>
-      {!entries.length
-        ? null
-        : entries.map((e) => {
-            return (
-              <Box key={e.id}>
-                <Typography variant='body1'>
-                  {e.date} {e.description}
-                </Typography>
-                {e.diagnosisCodes ? (
-                  <List>
-                    {e.diagnosisCodes.map((dc) => {
-                      return (
-                        <ListItem disablePadding key={dc}>
-                          <ListItemIcon>
-                            <FiberManualRecordIcon fontSize='small' />
-                          </ListItemIcon>
-                          <ListItemText primary={dc} />
-                        </ListItem>
-                      );
-                    })}
-                  </List>
-                ) : null}
-              </Box>
-            );
-          })}
+      {entries.map((e) => {
+        return (
+          <Box key={e.id}>
+            <Typography variant='body1'>
+              {e.date} {e.description}
+            </Typography>
+            {e.diagnosisCodes && e.diagnosisCodes.length && diagnos.length ? (
+              <List>
+                {e.diagnosisCodes.map((dc) => {
+                  return (
+                    <ListItem disablePadding key={dc}>
+                      <ListItemIcon>
+                        <FiberManualRecordIcon fontSize='small' />
+                      </ListItemIcon>
+                      <ListItemText
+                        primary={`${dc} ${
+                          diagnos.find((d) => d.code === dc)?.name || ''
+                        }`}
+                      />
+                    </ListItem>
+                  );
+                })}
+              </List>
+            ) : null}
+          </Box>
+        );
+      })}
     </Stack>
   );
 };
